0070a9e0ce06bd7144ec2cceb7d5cc0a5f5881a7
Adrian <a.valladares.d@gmail.com>
2023-12-03 16:32:14 +0100

    Initial commit

---
+++ test/maps_info_extractor_app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MapsInfoExtractorApp from '../lib/maps_info_extractor_app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/maps_info_extractor_app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MapsInfoExtractorApp.MapsInfoExtractorAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
